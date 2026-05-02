241efbbdf144d8d2ed7c37a286b3e89dee5fe545
Tim Rivoli <36770881+TimRivoli@users.noreply.github.com>
2026-05-02 16:15:03 -0700

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.android.application") version "9.2.0" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
+    id("com.google.devtools.ksp") version "2.1.20-1.0.32" apply false
 }
